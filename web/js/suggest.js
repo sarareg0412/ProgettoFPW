ff9d817204a27cb99a928502be4785d5fad8206b
Sara <Sara@DESKTOP-1A9GI24>
2019-06-12 18:08:35 +0200

    quasi finito task 3

--- web/js/suggest.js
+++ web/js/suggest.js
@@ -4,44 +4,52 @@
  * and open the template in the editor.
  */
 
-function createElement(autore){
+function createElement(autore) {
 
-    var nome = $("<li>").html(autore.nome+", "+autore.cognome+" ("+autore.id+")");
-    return $("<a>").attr("class", "div-autori").append(nome);
-    
+    var nome = $("<li>").html(autore.nome + ", " + autore.cognome + " (" + autore.id + ")").attr("class", "autore");
+    nome.attr("id",autore.nome + ", " + autore.cognome + " (" + autore.id + ")" );
+    nome.click(function (event) {
+        $("#author").text($(this).attr("id"));
+    });
+    return $("<a>").append(nome);
+
 }
 
-function stateSuccess(data){ //Risposta HTTP con positiva
+function stateSuccess(data) { //Risposta HTTP con positiva
     //Quindi data è la risposta (array di json) che ho costruito nel JSP
 
     var autoriDiv = $("#div-autori");
 
     $(autoriDiv).empty();
 
-    for(var instance in data){
+    for (var instance in data) {
         $(autoriDiv).append(createElement(data[instance]));
     }
-    
+
 }
 
-function stateFailure(data, state){
+function stateFailure(data, state) {
     console.log(state);
 }
 
-$(document).ready(function() { //Quando è pronto il DOM
-    
-    $("#author").keyup(function(event){ //Viene scritto qualcosa nella textarea "author"
-      
-       $.ajax({
-          url: "suggest.json", 
-          data: {cmd: "search",
-                 toSearch: event.target.value
-          },
-          dataType: 'json',
-          success: function(data, state){stateSuccess(data);},
-          error: function(data, state){stateFailure(data, state);}
-       });
-        
+$(document).ready(function () { //Quando è pronto il DOM
+
+    $("#author").keyup(function (event) { //Viene scritto qualcosa nella textarea "author"
+
+        $.ajax({
+            url: "suggest.json",
+            data: {cmd: "search",
+                toSearch: event.target.value
+            },
+            dataType: 'json',
+            success: function (data, state) {
+                stateSuccess(data);
+            },
+            error: function (data, state) {
+                stateFailure(data, state);
+            }
+        });
+
     });
-    
+
 });
